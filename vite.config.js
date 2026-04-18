54f66a8aa99dc2af01bf6aa24dff0f95f292c524
Reinhardt lagat <reinhardtlagat@gmail.com>
2026-04-18 12:12:20 +0300

    migrate to vite, fix tailwind nesting

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/css/site.css',
+                'resources/css/tailwind.css',
                 'resources/js/site.js',
 
                 // Control Panel assets.
