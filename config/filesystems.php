3b244b230105258cfce09bde45de792b9a01a7e9
Reinhardt lagat <reinhardtlagat@gmail.com>
2026-04-18 12:46:48 +0300

    fix assets disk url

--- config/filesystems.php
+++ config/filesystems.php
@@ -60,7 +60,7 @@ return [
         'assets' => [
             'driver' => 'local',
             'root' => public_path('assets'),
-            'url' => '/assets',
+            'url' => env('APP_URL').'/assets',
             'visibility' => 'public',
             'throw' => false,
         ],
